63e711b57f92e4c3c066900a9096a9231e5ce61a
Keawa Rozet <keawa.rozet@yahoo.com>
2017-11-20 17:35:07 -0800

    KR-68 All files are completely updated

---
+++ packages/GameState/EndState.java
@@ -0,0 +1,45 @@
+
+package GameState;
+
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class EndState extends GameState{
+    
+    private Font font;
+    
+    
+    public EndState(GameStateManager gsm) {
+        this.gsm = gsm;
+        init();
+    }
+
+    @Override
+    protected void init() {
+        font = new Font("Arial", Font.PLAIN, 40);
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void draw(Graphics2D gEndScreen, Graphics2D nullValue, Graphics2D nullValue2) {
+       gEndScreen.setFont(font);
+       gEndScreen.drawString("Game Over!", 300, 100);
+       gEndScreen.drawString("Press Escape to go back to the menu", 200, 300);
+    }
+
+    @Override
+    public void keyPressed(int k) {
+        if (k == KeyEvent.VK_ESCAPE) {
+            gsm.setState(GameStateManager.MENUSTATE);
+        }
+    }
+
+    @Override
+    public void keyReleased(int k) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
