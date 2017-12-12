fc94f5a0276e943c7dd851a558e7af9f186555c2
Jeffrey <Jeffrey@Jeffrey-PC>
2017-12-12 00:56:54 -0800

    Minor fixes for mini map

--- packages/TankGame/MiniMap.java
+++ packages/TankGame/MiniMap.java
@@ -1,8 +1,9 @@
 
 package TankGame;
 
+import BlockMap.Block;
+import BlockMap.BlockMap;
 import GameObjects.Player;
-import Main.GamePanel;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -14,12 +15,15 @@ public class MiniMap {
   private Player p1;
   private Player p2;
   
-  private GamePanel gamePanel;
+  private BlockMap blockMap;
   
   private double miniMapRatioHeight;
   private double miniMapRatioWidth;
   private double height;
   private double width;
+  public static final int EMPTY_TILE = 0;
+  public static final int BREAKABLE = 1;
+  public static final int UNBREAKABLE = 2;
   
   public MiniMap(Player p1, Player p2) {
     this.p1 = p1;
@@ -28,16 +32,12 @@ public class MiniMap {
     width = 250;
     height = 150;
     
-    gamePanel = new GamePanel();
+    blockMap = new BlockMap();
     
-    miniMapRatioHeight = height / GamePanel.HEIGHT;
-    miniMapRatioWidth = width / GamePanel.WIDTH;
-    System.out.println(width);
-    System.out.println(height);
-    System.out.println(GamePanel.HEIGHT);
-    System.out.println(GamePanel.WIDTH);
-    System.out.println(miniMapRatioHeight);
-    System.out.println(miniMapRatioWidth);
+    
+    
+    miniMapRatioHeight = height / (blockMap.getHeight());
+    miniMapRatioWidth = width / (blockMap.getWidth());
     
   }
   
@@ -48,13 +48,16 @@ public class MiniMap {
   
   public void draw(Graphics2D m) {
     m.setColor(Color.BLACK);
-    m.fillRect(375, 100, (int) width, (int) height);
+    m.fillRect(375, 10, (int) width, (int) height); //(x-pos, y-pos, width, height)
+
     
     m.setColor(Color.RED); //p1 color
-    m.fillOval((int)(375 + p1.getX() * miniMapRatioWidth),(int)(100 + p1.getY() * miniMapRatioHeight), 10, 10);
+    m.fillOval((int)(375 + p1.getX() * miniMapRatioWidth),(int)(10 + p1.getY() * miniMapRatioHeight), 10, 10);
     
     m.setColor(Color.BLUE); //p1 color
-    m.fillOval((int)(375 + p2.getX() * miniMapRatioWidth),(int)(100 + p2.getY() * miniMapRatioHeight), 10, 10);
+    m.fillOval((int)(375 + p2.getX() * miniMapRatioWidth),(int)(10 + p2.getY() * miniMapRatioHeight), 10, 10);
+    System.out.println(p1.getX());
+    System.out.println(p2.getY());
     
   }
   
